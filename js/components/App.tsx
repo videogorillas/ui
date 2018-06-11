bfc799126150a923bf1298fb86c051a9cf41ce8b
zhukovka <helen.zhukova@gmail.com>
2018-06-11 12:29:37 +0300

    ranges from jsonl

--- js/components/App.tsx
+++ js/components/App.tsx
@@ -3,52 +3,15 @@ import './app.css';
 import Ranges from "./Ranges";
 import {LabeledRange} from "../models/Range";
 import {fromEvent} from "rxjs/index";
+import {jsonlIterator} from "../utils/FetchUtils";
 
-function getJson<T> (): Promise<T> {
-    return fetch("LFA123.mp4.out.json").then(r => r.json());
-}
-
-function normalizeToRanges (json: JsonResult[]) {
-    //[[0, [0.8, 0.1]], [1, [0.6, 0.3]], [2, [0.8, 0.1]]] -> [{start: 0, end: 2, label: '0'}]
-    return json.reduce((ranges: LabeledRange[], item, i, arr) => {
-        let range: LabeledRange;
-        const predictions = item[1];
-        const max = Math.max(...predictions);
-        const index = predictions.indexOf(max);
-        if (!ranges.length) {
-            range = {
-                start : item[0],
-                end : item[0] + 1,
-                label : `${index}`
-            };
-            ranges.push(range);
-        } else {
-            range = ranges[ranges.length - 1];
-            if (`${index}` === range.label) {
-                range.end = item[0] + 1;
-            } else {
-                range = {
-                    start : item[0],
-                    end : item[0] + 1,
-                    label : `${index}`
-                };
-                ranges.push(range);
-            }
-        }
-        return ranges;
-    }, []);
-}
-
-interface JsonResult {
-    '0': number;
-    '1': [number, number]
-}
+type JsonResult = [number, [number, number]];
 
 interface AppProps {
 }
 
 interface AppState {
-    fn: number;
+    frame: number;
     total: number;
     ranges: LabeledRange[];
 }
@@ -56,27 +19,61 @@ interface AppState {
 export default class App extends React.Component<AppProps, AppState> {
 
     state = {
-        fn : 42,
+        frame : 0,
         total : 0,
         ranges : [] as LabeledRange[]
     };
-
-    changeFn = (fn: number) => {
-        this.setState({fn});
+    classes = ['no smoking', 'smoking'];
+    changeFn = (frame: number) => {
+        this.setState({frame});
     };
     private player: VG.Player;
+    private predictions: number[] = [];
+
+    async fetchJsonl () {
+        const url = 'cruz-smoking.jsonl';
+        const ranges = [];
+        let total = 0;
+        this.predictions = [];
+        for await (const item of jsonlIterator(url)) {
+            total += 1;
+            let range: LabeledRange;
+            const predictions = item[1];
+            const max = Math.max(...predictions);
+            const index = predictions.indexOf(max);
+            this.predictions.push(index);
+            if (!ranges.length) {
+                range = {
+                    start : item[0],
+                    end : item[0] + 1,
+                    label : `${index}`
+                };
+                ranges.push(range);
+            } else {
+                range = ranges[ranges.length - 1];
+                if (`${index}` === range.label) {
+                    range.end = item[0] + 1;
+                } else {
+                    range = {
+                        start : item[0],
+                        end : item[0] + 1,
+                        label : `${index}`
+                    };
+                    ranges.push(range);
+                }
+            }
+        }
+        this.setState({ranges, total})
+    }
 
     componentDidMount () {
-        getJson<JsonResult[]>().then(json => {
-            // console.log(json);
-            this.setState({total : json.length, ranges : normalizeToRanges(json)})
-        });
+        this.fetchJsonl();
 
         const kdown = fromEvent<MouseEvent>(document, 'keydown');
 
         kdown.subscribe(e => {
             console.log(e);
-            let {fn} = this.state;
+            let {frame} = this.state;
             let step = 0;
             switch (e.code) {
                 case "ArrowRight":
@@ -86,22 +83,10 @@ export default class App extends React.Component<AppProps, AppState> {
                     step = -1;
                     break;
             }
-            fn += step;
-            this.setState({fn});
+            frame += step;
+            this.setState({frame});
         });
     }
-
-    ranges = [
-        {start : 0, end : 42, label : "kuku"},
-        {start : 42, end : 47, label : "kuku1"}, {
-            start : 47,
-            end : 57,
-            label : "kuku"
-        }, {
-            start : 57,
-            end : 87,
-            label : "kuku1"
-        }];
 
     containerRef = (el) => {
         if (el) {
@@ -112,42 +97,61 @@ export default class App extends React.Component<AppProps, AppState> {
                 theme : 'vg',
                 plugins : ['filmstrip']
             };
-            const player = new VG.Player(el, pConfig);
-
-            player.loadUrl("http://10.0.1.140/bstorage/home/chexov/testvideo/LFA.mp4", (err) => {
-                this.player = player;
-                console.log(err, "player");
-                player.addEventListener("timeupdate", this.onTimeUpdate);
-            });
+            this.setupPlayer(el, pConfig);
         }
     };
 
+    private setupPlayer (el, pConfig) {
+        const player = new VG.Player(el, pConfig);
+
+        // const mp4 = "http://10.0.1.140/bstorage/home/chexov/testvideo/LFA.mp4";
+        const mp4 = "http://blender.local/bstorage/datasets/vg_smoke/smoking_scenes/045%20-%20Penelope%20Cruz%20Smoking.mkv.mp4";
+        player.loadUrl(mp4, (err) => {
+            // VG.Captions.parseSubs(player.getTimeline(), "LFA123.mp4.out.srt", 'srt', (err, subs) => {
+            //     if (err) {
+            //         console.error("error parsing subs", err);
+            //         return;
+            //     }
+            //     console.log("SRT OK", subs);
+            //     player.addCaptions(subs);
+            //     player.play();
+            // });
+            this.player = player;
+            console.log(err, "player");
+            player.addEventListener("timeupdate", this.onTimeUpdate);
+        });
+    }
+
     onTimeUpdate = (ts) => {
-        console.log(ts);
-        this.setState({fn : ts.frame})
+        const frame = ts.frame;
+        console.log(this.predictions[frame]);
+        this.setState({frame})
     };
 
-    onSelectFrame = (fn) => {
+    onSelectFrame = (frame) => {
         if (this.player) {
-            this.player.seekFrame(fn);
+            this.player.seekFrame(frame);
         }
     };
 
     render () {
-        const {fn} = this.state;
+        const {frame} = this.state;
         return <div>
             <div ref={this.containerRef}>
 
             </div>
+            <div>
+                {this.classes[this.predictions[frame]]}
+            </div>
             {this.state.ranges.length > 0 ?
-                <Ranges pointer={fn}
+                <Ranges pointer={frame}
                         ranges={this.state.ranges} end={this.state.total}
                         onClick={this.onSelectFrame}
                 />
                 : null
             }
             <div>
-                Frame number <input type="number" value={fn} onChange={(e) => this.changeFn(+e.target.value)}/>
+                Frame number <input type="number" value={frame} onChange={(e) => this.changeFn(+e.target.value)}/>
             </div>
         </div>
     }

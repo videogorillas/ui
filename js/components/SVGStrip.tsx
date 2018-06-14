3ba9ac47cb129c914d51dcb1aa321e44d7bfd160
exipilis <malenko.andrii@gmail.com>
2018-06-14 21:25:51 +0300

    simplify formulas

--- js/components/SVGStrip.tsx
+++ js/components/SVGStrip.tsx
@@ -79,7 +79,7 @@ export default class SVGStrip extends React.Component<StripProps, StripState> {
         if (this.outSvg.current && zoom > 1) {
             const w = this.outSvg.current.clientWidth;//window
             const cur_trx = -this.innerG.current.transform.animVal[0].matrix.e;
-            const max_tr_x = w * (zoom - 1);
+            // const max_tr_x = w * (zoom - 1);
             // console.log('tr_x max_tr_x', Math.round(cur_trx), max_tr_x);
             const t1 = Math.max(0, cur_trx / w / zoom);
             const t2 = Math.min(1, (cur_trx + w) / w / zoom);
@@ -88,17 +88,19 @@ export default class SVGStrip extends React.Component<StripProps, StripState> {
             // console.log('videopos', videoPos);
             let new_trx = 0;
             if (videoPos < t1 + 0.1 / zoom) // move to the left of current window
-                if (videoPos < 0.1 / zoom) {
-                    new_trx = 0;
-                } else {
-                    new_trx = (videoPos * zoom - 0.1) * w;
-                }
+                new_trx = Math.max((videoPos * zoom - 0.1) * w, 0);
+            // if (videoPos < 0.1 / zoom) {
+            //     new_trx = 0;
+            // } else {
+            //     new_trx = (videoPos * zoom - 0.1) * w;
+            // }
             else if (videoPos > t2 - 0.1 / zoom) // move to the right of current window
-                if (videoPos > 1 - 0.1 / zoom) {
-                    new_trx = max_tr_x;
-                } else {
-                    new_trx = (videoPos * zoom - 0.9) * w;
-                }
+                new_trx = Math.min((videoPos * zoom - 0.9) * w, w * (zoom - 1));
+            // if (videoPos > 1 - 0.1 / zoom) {
+            //     new_trx = max_tr_x;
+            // } else {
+            //     new_trx = (videoPos * zoom - 0.9) * w;
+            // }
 
             else
                 new_trx = cur_trx;
